bbd0fd2308357e2b5d4d4cd28ad7c06bc4983b6c
최규광 <kisay97@naver.com>
2016-05-26 10:11:33 +0900

    20160526-1011 : 셀렉트 처리

--- app/src/main/java/com/estsoft/contextmenuexample/MainActivity.java
+++ app/src/main/java/com/estsoft/contextmenuexample/MainActivity.java
@@ -3,7 +3,9 @@ package com.estsoft.contextmenuexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_refresh :
+                Toast.makeText(this, "refresh", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
