55e53b8f656fdfd6c16790ee1a8c71a9686e0566
최규광 <kisay97@naver.com>
2016-05-26 09:48:57 +0900

    20160526-0948 : 컨텍스트 메뉴

--- app/src/main/java/com/estsoft/contextmenuexample/MainActivity.java
+++ app/src/main/java/com/estsoft/contextmenuexample/MainActivity.java
@@ -11,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerForContextMenu(findViewById(android.R.id.content));
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()){
+            case android.R.id.content :
+                getMenuInflater().inflate(R.menu.context_menu, menu);
+                break;
+            default:
+                break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
     }
 }
